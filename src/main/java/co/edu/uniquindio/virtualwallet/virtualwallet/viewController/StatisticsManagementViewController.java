d874a1c065093ac6b5c72ecaa20059d4d7c2a09b
jorgeMontoya022 <jorgew.montoyat@uqvirtual.edu.co>
2024-11-02 01:10:21 -0500

    Update the class StatisticsManagementViewController

--- src/main/java/co/edu/uniquindio/virtualwallet/virtualwallet/viewController/StatisticsManagementViewController.java
+++ src/main/java/co/edu/uniquindio/virtualwallet/virtualwallet/viewController/StatisticsManagementViewController.java
@@ -1,10 +1,15 @@
 package co.edu.uniquindio.virtualwallet.virtualwallet.viewController;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 
 public class StatisticsManagementViewController {
 
@@ -28,28 +33,36 @@ public class StatisticsManagementViewController {
 
     @FXML
     void onHome(ActionEvent event) {
-
+        loadView("adminData-view.fxml");
     }
 
     @FXML
     void onOpenAverageBalance(ActionEvent event) {
-
+        loadView("graphic-average-balance-view.fxml");
     }
 
     @FXML
     void onOpenCommonExpenses(ActionEvent event) {
-
+        loadView("graphic-common-expenses-view.fxml");
     }
 
     @FXML
     void onOpenFrecuentTransactions(ActionEvent event) {
+        loadView("graphic-transactions-frecuentes-view.fxml");
+    }
 
+    private void loadView(String fxmlFile) {
+        try {
+
+            Parent newView = FXMLLoader.load(getClass().getResource(fxmlFile));
+            Scene currentScene = btnHome.getScene();
+            currentScene.setRoot(newView);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void initialize() {
-
-
     }
-
 }
