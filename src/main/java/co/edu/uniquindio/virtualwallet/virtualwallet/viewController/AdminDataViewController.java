6bef97aa5447ac63816ca4649ba96349b9089f70
jorgeMontoya022 <jorgew.montoyat@uqvirtual.edu.co>
2024-11-02 12:27:12 -0500

    update adminDataViewController

--- src/main/java/co/edu/uniquindio/virtualwallet/virtualwallet/viewController/AdminDataViewController.java
+++ src/main/java/co/edu/uniquindio/virtualwallet/virtualwallet/viewController/AdminDataViewController.java
@@ -2,12 +2,20 @@ package co.edu.uniquindio.virtualwallet.virtualwallet.viewController;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import co.edu.uniquindio.virtualwallet.virtualwallet.controller.AdministratorDataController;
+import co.edu.uniquindio.virtualwallet.virtualwallet.mapping.dto.AdministratorDto;
+import co.edu.uniquindio.virtualwallet.virtualwallet.model.Administrator;
+import co.edu.uniquindio.virtualwallet.virtualwallet.utils.Session;
+import co.edu.uniquindio.virtualwallet.virtualwallet.viewController.services.IUserManagementViewController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class AdminDataViewController {
+public class AdminDataViewController extends CoreViewController implements IUserManagementViewController<AdministratorDto> {
+    Administrator person;
+    AdministratorDataController administratorDataController;
 
     @FXML
     private ResourceBundle resources;
@@ -46,24 +54,49 @@ public class AdminDataViewController {
     private TextField txtRegistrationDate;
 
     @FXML
-    void onCloseSession(ActionEvent event) {
+    public void onLogout(ActionEvent event) {
+        Session.getInstance().closeSession();
+        browseWindow("/view/startup-view.fxml", "BuckTrack", event);
 
     }
 
     @FXML
-    void onGoDashboard(ActionEvent event) {
+    public void onGoDashboard(ActionEvent event) {
+        browseWindow("/view/administrator-container-view.fxml", "Dashboard", event);
 
     }
 
     @FXML
-    void onUpdate(ActionEvent event) {
+    public void onUpdate(ActionEvent event) {
 
     }
 
     @FXML
-    void initialize() {
+    public void initialize() {
+        person = (Administrator) Session.getInstance().getPerson();
+        administratorDataController = new AdministratorDataController();
+        initView();
 
+    }
 
+    @Override
+    public void initView() {
+        showInformation();
+    }
+
+    private void showInformation() {
+        txtId.setText(person.getId());
+        txtAdminName.setText(person.getFullName());
+        txtEmail.setText(person.getEmail());
+        txtPassword.setText(person.getEmail());
+        txtPhone.setText(person.getPhoneNumber());
+        txtDateBirth.setText(String.valueOf(person.getBirthDate()));
+        txtRegistrationDate.setText(String.valueOf(person.getRegistrationDate()));
+    }
+
+    @Override
+    public boolean validateData(AdministratorDto object) {
+        return false;
     }
 
 }
